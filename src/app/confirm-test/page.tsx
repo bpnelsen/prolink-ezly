e3df9deb781d0499a49ef2d0137bad0e4d783516
Brian Nelsen <brian@prolink-ezly.com>
2026-03-27 21:17:04 -0700

    feat: add static confirm-test page

---
+++ src/app/confirm-test/page.tsx
@@ -0,0 +1,8 @@
+export default function ConfirmTest() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Static Test Page Works!</h1>
+      <p>If you can see this, static routes are fine.</p>
+    </div>
+  );
+}
